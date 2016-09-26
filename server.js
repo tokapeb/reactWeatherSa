222f76cf1001c14d0f88a20797ae03964d7ab457
tokapeb <tokap@gmail.com>
2016-09-26 08:01:05 +0200

    Fix typo.

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 3000;
 app.use(function (req, res, next) {
   if (req.headers['x-frowarded-proto'] === 'http') {
     next();
-  } esle {
+  } else {
     res.redirect('http://' + req.hostname + req.url);
   }
 });
